9fdc32fa53537ae4fa75c3616500759387335c25
lucidrains <lucidrains@gmail.com>
2025-11-02 06:41:15 -0800

    take care of the aux loss

--- ultra_mem/ultra_mem.py
+++ ultra_mem/ultra_mem.py
@@ -1,4 +1,5 @@
 import torch
+from torch import tensor
 import torch.nn.functional as F
 from torch.nn import Parameter, Module, ModuleList
 
@@ -29,17 +30,46 @@ class UltraMem(Module):
         dim_queries_keys = 128, # think this is what PKM uses
         core_rank = 2,          # the tucker decomposition core rank
         core_heads = 2,         # number of cores / heads
+        core_aux_loss_margin = 0.15,
+        aux_loss_weight = 0.1,
     ):
         super().__init__()
 
-        # their tucker decompose core is 2x2
+        # their tucker decomposed core is 2x2
         # learned e2e with an auxiliary loss
 
         self.core = Parameter(torch.randn(core_heads, core_rank, core_rank) * 1e-2)
 
+        # auxiliary loss on the core
+
+        self.core_aux_loss_margin = core_aux_loss_margin
+        self.aux_loss_weight = aux_loss_weight
+
+        self.register_buffer('zero', tensor(0.), persistent = False)
+
     def forward(
         self,
         tokens,
-        trainable_sparse_mask = None # bool[num_memories,]
+        trainable_sparse_mask = None, # bool[num_memories,]
+        return_aux_loss = None
     ):
-        return tokens
+
+        # svd
+
+        u, s, v = torch.svd(self.core)
+
+        # aux loss on singular values
+
+        return_aux_loss = default(return_aux_loss, self.training)
+
+        aux_loss = self.zero
+
+        if return_aux_loss:
+            non_first_singular_values = s[:, 1:]
+
+            aux_loss = F.relu(non_first_singular_values - self.core_aux_loss_margin).pow(2).mean() # eq (12)
+            aux_loss = aux_loss * self.aux_loss_weight
+
+        # returning
+
+        return tokens, aux_loss
