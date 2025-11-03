ce2c38fa1dab55c0820929afccc863c003c2be66
lucidrains <lucidrains@gmail.com>
2025-11-03 05:53:30 -0800

    Csordas proposed relu as an improved non-competitive activation fn, but in the paper not mentioned, so just let (independent) researchers know

--- tests/test_ultra_mem.py
+++ tests/test_ultra_mem.py
@@ -2,14 +2,17 @@ import pytest
 param = pytest.mark.parametrize
 
 import torch
+from torch import nn
 
 @param('value_expansion', (1, 2))
 @param('core_heads', (1, 2))
 @param('layers_for_mem_init', (None, 12))
+@param('score_activation', (nn.Identity(), nn.ReLU()))
 def test_ultra_mem(
     value_expansion,
     core_heads,
-    layers_for_mem_init
+    layers_for_mem_init,
+    score_activation
 ):
     from ultra_mem.ultra_mem import UltraMem
 
@@ -19,7 +22,8 @@ def test_ultra_mem(
         512,
         value_expansion = value_expansion,
         core_heads = core_heads,
-        layers_for_mem_init = layers_for_mem_init
+        layers_for_mem_init = layers_for_mem_init,
+        score_activation = score_activation
     )
 
     out, aux_loss = ultra_mem(x)
